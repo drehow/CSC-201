c56a93cd15983efcf7a695c7ecaa8a65a16c59bc
Drew Howell <Drew.Howell@gatewayloan.com>
2021-01-22 14:08:31 -0600

    Updated files.

--- 2020_Fall/20201030_SVM/svm_init.R
+++ 2020_Fall/20201030_SVM/svm_init.R
@@ -1,178 +1,75 @@
-set.seed(10)
+##################################################################################
+### PACKAGES ###
+##################################################################################
+library(rtweet)
+library(dplyr)
+library(tidyr)
+library (tidytext)
+library(httpuv)
+library(ggplot2)
+library(lubridate)
+library(chron)
 
-# Attach Packages
-library(tidyverse)    # data manipulation and visualization
-library(kernlab)      # SVM methodology
-library(e1071)        # SVM methodology
-library(ISLR)         # contains example data set "Khan"
-library(RColorBrewer) # customized coloring of plots
+##################################################################################
+###TWEET ACCESS ###
+##################################################################################
 
-# Construct sample data set - completely separated
-x <- matrix(rnorm(20*2), ncol = 2)
-y <- c(rep(-1,10), rep(1,10))
-x[y==1,] <- x[y==1,] + 3/2
-dat <- data.frame(x=x, y=as.factor(y))
+## label key and secret key
+api_key <- "7lIgKeBPEe36d1daOhJKQ4I5i"
+api_secret_key <- "68UmK5FGxNMoiQdjYirve2TmHoRF0gIPsaYlavXjYRe4c8HyHS"
 
-# Plot data
-ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) + 
-  geom_point(size = 2) +
-  scale_color_manual(values=c("#000000", "#FF0000")) +
-  theme(legend.position = "none")
+### Create Token for API access ###
+token <- create_token(
+  app = "Soederstocks",
+  consumer_key = api_key,
+  consumer_secret = api_secret_key)
 
+##################################################################################
+###uploading TWEET data ###
+##################################################################################
 
-# Fit Support Vector Machine model to data set
-svmfit <- svm(y~., data = dat, kernel = "linear", scale = FALSE)
-# Plot Results
-plot(svmfit, dat)
+## upload aapl tweets
+AAPLtweets <- rtweet::search_tweets(q = '$AAPL', 
+                                   n = 10000,
+                                   since = "2020-11-10",
+                                   until = "2020-11-19")
 
-# fit model and produce plot
-kernfit <- ksvm(x, y, type = "C-svc", kernel = 'vanilladot')
-plot(kernfit, data = x)
-
-
-# Construct sample data set - not completely separated
-x <- matrix(rnorm(20*2), ncol = 2)
-y <- c(rep(-1,10), rep(1,10))
-x[y==1,] <- x[y==1,] + 1
-dat <- data.frame(x=x, y=as.factor(y))
-
-# Plot data set
-ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) + 
-  geom_point(size = 2) +
-  scale_color_manual(values=c("#000000", "#FF0000")) +
-  theme(legend.position = "none")
-
-
-
-# Fit Support Vector Machine model to data set
-svmfit <- svm(y~., data = dat, kernel = "linear", cost = 10)
-# Plot Results
-plot(svmfit, dat)
-
-
-# Fit Support Vector Machine model to data set
-kernfit <- ksvm(x,y, type = "C-svc", kernel = 'vanilladot', C = 100)
-# Plot results
-plot(kernfit, data = x)
-
-
-tune.out <- tune(svm, y~., data = dat, kernel = "linear",
-                 ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
-# extract the best model
-(bestmod <- tune.out$best.model)
-
-
-# Create a table of misclassified observations
-ypred <- predict(bestmod, dat)
-(misclass <- table(predict = ypred, truth = dat$y))
-
-
-
-# construct larger random data set
-x <- matrix(rnorm(200*2), ncol = 2)
-x[1:100,] <- x[1:100,] + 2.5
-x[101:150,] <- x[101:150,] - 2.5
-y <- c(rep(1,150), rep(2,50))
-dat <- data.frame(x=x,y=as.factor(y))
-
-# Plot data
-ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) + 
-  geom_point(size = 2) +
-  scale_color_manual(values=c("#000000", "#FF0000")) +
-  theme(legend.position = "none")
-
-
-# set pseudorandom number generator
-set.seed(123)
-# sample training data and fit model
-train <- base::sample(200,100, replace = FALSE)
-svmfit <- svm(y~., data = dat[train,], kernel = "radial", gamma = 1, cost = 1)
-# plot classifier
-plot(svmfit, dat)
-
-
-# Fit radial-based SVM in kernlab
-kernfit <- ksvm(x[train,],y[train], type = "C-svc", kernel = 'rbfdot', C = 1, scaled = c())
-# Plot training data
-plot(kernfit, data = x[train,])
-
-
-# tune model to find optimal cost, gamma values
-tune.out <- tune(svm, y~., data = dat[train,], kernel = "radial",
-                 ranges = list(cost = c(0.1,1,10,100,1000),
-                               gamma = c(0.5,1,2,3,4)))
-# show best model
-tune.out$best.model
-
-
-
-# validate model performance
-(valid <- table(true = dat[-train,"y"], pred = predict(tune.out$best.model,
-                                                       newx = dat[-train,])))
-
-
-# construct data set
-x <- rbind(x, matrix(rnorm(50*2), ncol = 2))
-y <- c(y, rep(0,50))
-x[y==0,2] <- x[y==0,2] + 2.5
-dat <- data.frame(x=x, y=as.factor(y))
-# plot data set
-ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) + 
-  geom_point(size = 2) +
-  scale_color_manual(values=c("#000000","#FF0000","#00BA00")) +
-  theme(legend.position = "none")
-
-
-# fit model
-svmfit <- svm(y~., data = dat, kernel = "radial", cost = 10, gamma = 1)
-# plot results
-plot(svmfit, dat)
-
-
-#construct table
-ypred <- predict(svmfit, dat)
-(misclass <- table(predict = ypred, truth = dat$y))
-
-
-# fit and plot
-kernfit <- ksvm(as.matrix(dat[,2:1]),dat$y, type = "C-svc", kernel = 'rbfdot', 
-                C = 100, scaled = c())
-
-# Create a fine grid of the feature space
-x.1 <- seq(from = min(dat$x.1), to = max(dat$x.1), length = 100)
-x.2 <- seq(from = min(dat$x.2), to = max(dat$x.2), length = 100)
-x.grid <- expand.grid(x.2, x.1)
-
-# Get class predictions over grid
-pred <- predict(kernfit, newdata = x.grid)
-
-# Plot the results
-cols <- brewer.pal(3, "Set1")
-plot(x.grid, pch = 19, col = adjustcolor(cols[pred], alpha.f = 0.05))
-
-classes <- matrix(pred, nrow = 100, ncol = 100)
-contour(x = x.2, y = x.1, z = classes, levels = 1:3, labels = "", add = TRUE)
-
-points(dat[, 2:1], pch = 19, col = cols[predict(kernfit)])
+##split Created at into date and time
+AAPLtweets$date <- as.Date(AAPLtweets$created_at, format = "%Y / %m / %d")  
+##AAPLtweets$time <- chron(AAPLtweets$created_at, format = "%H:%M") ## needs work
 
 
 
 
-# fit model
-dat <- data.frame(x = Khan$xtrain, y=as.factor(Khan$ytrain))
+##################################################################################
+###STOCKS DATA ###
+##################################################################################
 
-(out <- svm(y~., data = dat, kernel = "radial", cost=10))
+## import data
+AAPLstock <- readr::read_csv('C:/Users/drew.howell/Desktop/AAPL.txt')
 
-# # tune model to find optimal cost, gamma values
-# tune.out <- tune(svm, y~., data = dat, kernel = 'linear',
-#                  ranges = list(cost = c(0.1,1)))
-# show best model
-# tune.out$best.model
+## define COL names
+names(AAPLstock) <- c("date", "time", "adj_open" , "adj_high", "adj_low", "adj_close" , "volume")
 
-# check model performance on training set
-table(out$fitted, dat$y)
+##set date as date
+AAPLstock$date <- as.Date(AAPLstock$date, format = "%m / %d / %Y")
 
-# validate model performance
-dat.te <- data.frame(x=Khan$xtest, y=as.factor(Khan$ytest))
-pred.te <- predict(out, newdata=dat.te)
-table(pred.te, dat.te$y)
+##split date to 3 seperate col
+AAPLstock <- mutate(AAPLstock,
+                 year = as.numeric(format(AAPLstock$date, format = "%Y")),
+                 month = as.numeric(format(AAPLstock$date, format = "%m")),
+                 day = as.numeric(format(AAPLstock$date, format = "%d")))
+
+## filter to selected time period
+AAPLstock <- filter(AAPLstock, year == 2020, month == 11, day >= 10)
+
+## delete date column
+AAPLstock <- select(AAPLstock, !date)
+
+
+## create new columns <- %change, range (variation), and %range (volatility?)
+AAPLstock <- mutate(AAPLstock, perc_change = ((adj_close - adj_open)/adj_open),
+                    range = (adj_high - adj_low),
+                    perc_range = ((adj_high - adj_low)/ adj_open))
+
+
